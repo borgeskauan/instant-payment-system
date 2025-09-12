466be8e73305c164ab62ae7febea1dad734d0ca7
tyrael <borges.kauan.martins@gmail.com>
2025-09-11 21:45:44 -0300

    Creating PSP project

---
+++ payment-service-provider/src/test/java/br/kauan/paymentserviceprovider/PaymentServiceProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.kauan.paymentserviceprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
